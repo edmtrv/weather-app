fd805d3338278e46005aa52a1f0dbe8c29e07cbf
Emil Dimitrov <edimitrov1@gmail.com>
2020-03-06 19:21:56 +0200

    Get weather data for a location

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import LocationSearch from './LocationSearch';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const App = () => {
   const [location, setLocation] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    if (location) {
+      (async location => {
+        const response = await axios.get(
+          `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`
+        );
+        setWeatherData(response.data);
+      })(location);
+    }
+  }, [location]);
 
   const onSearchLocation = term => {
     setLocation(term);
@@ -12,7 +27,7 @@ const App = () => {
     <div className="ui fluid container">
       <div className="ui segment">
         <LocationSearch onSearchLocation={onSearchLocation} />
-        {location}
+        {weatherData ? weatherData.weather[0].description : null}
       </div>
     </div>
   );
