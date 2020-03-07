625cd547246ecdc2bad6871654538e4ba613c629
Emil Dimitrov <edimitrov1@gmail.com>
2020-03-08 00:17:46 +0200

    Updates

---
+++ precache-manifest.8a410da2d77877b511fde7b54701207d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06583031474ba9f894e36f5e589bf2f6",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "8d8d10601fa7990d2855",
+    "url": "/weather-app/static/js/2.3da2b34a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather-app/static/js/2.3da2b34a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7806f4a0b036baf4653c",
+    "url": "/weather-app/static/js/main.faae58cb.chunk.js"
+  },
+  {
+    "revision": "089fb1fee07ab9da1ce8",
+    "url": "/weather-app/static/js/runtime-main.029e69d5.js"
+  }
+]);
